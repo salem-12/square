5f228ee80b01b9f7757ceb818c7b3ae0ee96a0a1
salem-12 <salsalem02>
2020-04-16 15:40:41 -0400

    First commit

---
+++ src/square.java
@@ -0,0 +1,19 @@
+/**
+//Start program
+        declare a variable int for number and square
+        initialize i is equal to 1 and increment by 1
+        Print out the message
+        stop program
+*/
+public class square {
+        public static void main (String [] args)
+        {
+            int number, Square;
+            for (int i= 1; i <= 10; i++)
+            {
+                System.out.println(i+" "+i*i);
+            }
+        }
+    }
+
+
